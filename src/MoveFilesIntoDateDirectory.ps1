d972d9f0a076971d9acf3f8e49f9f08ad9a2c8df
deadlydog <deadlydog@hotmail.com>
2020-10-31 15:10:23 -0600

    feat: Add parameter validations. feat: Add Force parameter to allow overwriting files

--- src/MoveFilesIntoDateDirectory.ps1
+++ src/MoveFilesIntoDateDirectory.ps1
@@ -5,17 +5,23 @@
 Param
 (
 	[Parameter(Mandatory = $true, HelpMessage = 'The directory to look for files to move in.')]
+	[ValidateNotNullOrEmpty()]
 	[System.IO.DirectoryInfo] $SourceDirectoryPath = 'C:\dev\Git\MoveFilesIntoDateDirectories\FilesToSearch',
 
 	[Parameter(Mandatory = $false, HelpMessage = 'How many subdirectories deep the script should search for files to move. Default is no limit.')]
+	[ValidateRange(0, [int]::MaxValue)]
 	[int] $SourceDirectoryDepthToSearch = [int]::MaxValue,
 
 	[Parameter(Mandatory = $true, HelpMessage = 'The directory to create the date-named directories in and move the files to.')]
+	[ValidateNotNullOrEmpty()]
 	[System.IO.DirectoryInfo] $TargetDirectoryPath = 'C:\dev\Git\MoveFilesIntoDateDirectories\SortedFiles',
 
-	[Parameter(HelpMessage = 'The scope at which directories should be created. Accepted values include "Hour", "Day", "Month", or "Year". e.g. If you specify "Day" files will be moved from the `SourceDirectoryPath` to `TargetDirectoryPath\yyyy-MM-dd`.')]
+	[Parameter(Mandatory = $false, HelpMessage = 'The scope at which directories should be created. Accepted values include "Hour", "Day", "Month", or "Year". e.g. If you specify "Day" files will be moved from the `SourceDirectoryPath` to `TargetDirectoryPath\yyyy-MM-dd`.')]
 	[ValidateSet('Hour', 'Day', 'Month', 'Year')]
-	[string] $TargetDirectoryDateScope = 'Day'
+	[string] $TargetDirectoryDateScope = 'Day',
+
+	[Parameter(Mandatory = $false, HelpMessage = 'If provided, the script will overwrite existing files instead of reporting an error the the file already exists.')]
+	[switch] $Force
 )
 
 Process
@@ -36,7 +42,7 @@ Process
 
 			[string] $filePath = $file.FullName
 			Write-Information "Moving file '$filePath' into directory '$dateDirectoryPath'."
-			Move-Item -Path $filePath -Destination $dateDirectoryPath
+			Move-Item -Path $filePath -Destination $dateDirectoryPath -Force:$Force
 		}
 }
 

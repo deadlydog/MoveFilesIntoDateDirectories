b6ad5339d034212475b8f5769e5a3ad4a35619bb
deadlydog <deadlydog@hotmail.com>
2022-10-19 11:06:50 -0600

    feat: Use more self-explanatory values in Invoke script

--- Changelog.md
+++ Changelog.md
@@ -5,6 +5,8 @@
 features:
 
 - Add support for more special characters in filenames, such as brackets, to address [issue #2](https://github.com/deadlydog/MoveFilesIntoDateDirectories/issues/2).
+- Update Invoke script values to be more descriptive and self-explanatory.
+
 ## v1.0.0 - November 1, 2020
 
 Initial script.
--- src/Invoke-MoveFilesIntoDateDirectories.ps1
+++ src/Invoke-MoveFilesIntoDateDirectories.ps1
@@ -3,9 +3,9 @@
 
 # Specify the parameters to call the cmdlet with.
 [hashtable] $scriptParameters = @{
-	SourceDirectoryPath = 'C:\Dans\Imported Photos\NotByDateYet'
-	#SourceDirectoryDepthToSearch = 2
-	TargetDirectoryPath = 'C:\Dans\Imported Photos'
+	SourceDirectoryPath = 'C:\SourceDirectory\WithFilesToMove'
+	#SourceDirectoryDepthToSearch = 2	# Default is to search all subdirectories.
+	TargetDirectoryPath = 'C:\TargetDirectory\ToMoveFilesInto'
 	TargetDirectoriesDateScope = 'Day'	# Hour, Day, Month, or Year
 	Force = $false
 }
